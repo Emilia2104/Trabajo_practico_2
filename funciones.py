64ffa4e0a611c88c1c38684c81de66fce947608a
Camicabrera <ccabreraroble@udesa.edu.ar>
2026-05-10 13:40:32 -0300

    Create funciones.py

---
+++ funciones.py
@@ -0,0 +1,12 @@
+def solicitud()-> str:
+    """
+    Solicita al usuario la ruta donde esta la imagen, si no es valida solicita su reingreso
+
+    Returns:
+        str: Retorna ruta valida de la imagen
+    """
+    ruta= input('Ingrese ruta donde esta la imagen a procesar:')
+    while not os.path.exists(ruta):
+        ruta= input('Ruta no valida, reingrese ruta')
+    return ruta
+
