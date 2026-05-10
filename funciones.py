87e451b2ea5b3dd4d42d914a0d2b398da4ee33b2
Camicabrera <ccabreraroble@udesa.edu.ar>
2026-05-10 13:41:18 -0300

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,3 +1,8 @@
+import numpy as np    
+import math
+import matplotlib.pyplot as plt 
+from PIL import Image  
+
 def solicitud()-> str:
     """
     Solicita al usuario la ruta donde esta la imagen, si no es valida solicita su reingreso
@@ -9,4 +14,5 @@ def solicitud()-> str:
     while not os.path.exists(ruta):
         ruta= input('Ruta no valida, reingrese ruta')
     return ruta
+
 
