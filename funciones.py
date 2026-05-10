063d5b8844ff5de7ffca2775dc973b18218f9596
Camicabrera <ccabreraroble@udesa.edu.ar>
2026-05-10 13:55:28 -0300

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -2,18 +2,13 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt 
 from PIL import Image  
+#PIXEL:
+def pixel(imagen, tamaño, niveles):
+    array_imagen = np.array (imagen)
+    alto= array_imagen.shape[0]
+    ancho= array_imagen.shape[1]
 
-def solicitud()-> str:
-    """
-    Solicita al usuario la ruta donde esta la imagen, si no es valida solicita su reingreso
 
-    Returns:
-        str: Retorna ruta valida de la imagen
-    """
-    ruta= input('Ingrese ruta donde esta la imagen a procesar:')
-    while not os.path.exists(ruta):
-        ruta= input('Ruta no valida, reingrese ruta')
-    return ruta
 
 
 
